583a6f7314c20dd6894a1434b319df321a5cf998
Cheba <dac42@tpu.ru>
2025-11-05 21:53:51 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server started: http://26.99.190.11:${PORT}`);
+});
