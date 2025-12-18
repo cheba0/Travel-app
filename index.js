038a6912b79da2916c82f126f4c45932d08bf2ad
Cheba <dac42@tpu.ru>
2025-12-18 22:07:11 +0700

    db

--- index.js
+++ index.js
@@ -123,5 +123,5 @@ try {
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server started: http://localhost:${PORT}`); //87.242.100.137 //localhost
+  console.log(`Server started: http://87.242.100.137:${PORT}`); //87.242.100.137 //localhost
 });
