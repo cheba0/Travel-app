83f890de153d34db54eea8236364dde570f0c827
Cheba <dac42@tpu.ru>
2026-02-13 23:28:40 +0700

    db

--- db.js
+++ db.js
@@ -7,11 +7,11 @@ const pool = new Pool({
   // db
   // localhost
   // 26.96.80.103
-  host: "localhost",
+  host: "db",
 
   // postgres
   // travel_app
-  database: "postgres",
+  database: "travel_app",
 
   password: "postgres",
   port: 5432,
